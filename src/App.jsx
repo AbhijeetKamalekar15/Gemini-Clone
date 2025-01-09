61c5ce9fb9de764ad7334de3e97aa5744554f4c1
Abhijeet_15 <118336271+AbhijeetKamalekar15@users.noreply.github.com>
2025-01-09 16:22:37 +0530

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+import Sidebar from "./components/Sidebar/Sidebar";
+import Main from "./components/Main/Main";
+
+const App = () => {
+  // const apiKey = process.env.GEMINI_API_KEY;
+  // console.log(apiKey);
+  
+
+  
+
+  return (
+    <>
+      <Sidebar />
+      <Main />
+    </>
+  );
+};
+
+export default App;
---
+++ src/config/gemini.js
@@ -0,0 +1,40 @@
+import {
+  GoogleGenerativeAI,
+  HarmCategory,
+  HarmBlockThreshold,
+} from "@google/generative-ai";
+
+
+const apiKey = "AIzaSyDmaLl4HpZxnshWKG7xCPZrb6aed_5fN5Q";
+console.log(apiKey);
+
+const genAI = new GoogleGenerativeAI(apiKey);
+
+const model = genAI.getGenerativeModel({
+  model: "gemini-2.0-flash-exp",
+});
+
+const generationConfig = {
+  temperature: 1,
+  topP: 0.95,
+  topK: 40,
+  maxOutputTokens: 8192,
+  responseMimeType: "text/plain",
+};
+
+async function run(prompt) {
+  const chatSession = model.startChat({
+    generationConfig,
+    history: [
+    ],
+  });
+
+  const result = await chatSession.sendMessage(prompt);
+  console.log(result.response.text());
+  return result.response.text();
+}
+
+export default run
+
+
+//AIzaSyDmaLl4HpZxnshWKG7xCPZrb6aed_5fN5Q
